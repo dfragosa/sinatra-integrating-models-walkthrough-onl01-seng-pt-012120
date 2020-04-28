ef005299398d52adda23d9cea32c35d059f0da23
Derric Fragosa <fderric@gmail.com>
2020-04-28 22:40:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
@@ -12,7 +13,7 @@ class App < Sinatra::Base
     @analyzed_text = TextAnalyzer.new(text_from_user)
     
     ##or just @analyzed_text = TextAnalyzer.new(params[:user_text])
-
+binding.pry
     erb :results
   end
 end
